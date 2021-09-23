166169e7790de98346b9c5bab8530ac889131c92
Jay Mathis <jmathis@x3technology.com>
2021-09-23 15:53:42 -0400

    fixes bug with empty mask

--- app/content-field-types/text/controller.js
+++ app/content-field-types/text/controller.js
@@ -1,3 +1,31 @@
-﻿app.controller('contentFieldTypeTextController', [function () {
+﻿app.controller('contentFieldTypeTextController', ['$scope', function ($scope) {
+
+    $scope.getMaskAttributes = function () {
+        if ($scope.contentField.options.inputMask) {
+            return "mask='" + $scope.contentField.options.inputMask + "' mask-restrict='reject' mask-validate='true'";
+        }
+        else {
+            return "nothing";
+        }
+    };
+
+    $scope.getMaskRestrict = function () {
+        if ($scope.contentField.options.inputMask) {
+            return "restrict";
+        }
+        else {
+            return "accept";
+        }
+    };
+
+    $scope.getMaskValidate= function () {
+        if ($scope.contentField.options.inputMask) {
+            return "true";
+        }
+        else {
+            return "false";
+        }
+    };
+
 }]);
 
